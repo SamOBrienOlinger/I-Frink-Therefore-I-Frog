4517de6b956ca03a67b6a5d4e1e06299ca7a061d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-14 14:46:06 +0000

    Set up env.py and update settings.py

--- codestar2022/settings.py
+++ codestar2022/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7-u@@li1o(*_^hm@cb&d_tar326^s7w*m5vh50!8j(wz8^7be'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,12 +80,12 @@ WSGI_APPLICATION = 'codestar2022.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = 'postgres://epljivykdhyyui:e8fb4c66a5f3a2fdbdf9cb9a93ed588f9b5f198b1eced1942b4cd65a21bf2f42@ec2-52-51-3-22.eu-west-1.compute.amazonaws.com:5432/ddimkaoejf2hmd'
+os.environ["SECRET_KEY"] = 'thebestsecretkeyever2000'
+
