fa3b95148a89f53357794b668847f50abec4bc94
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-11-14 13:10:04 +0000

    Django installed

--- codestar2022/asgi.py
+++ codestar2022/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PROJ_NAME project.
+ASGI config for codestar2022 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJ_NAME.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar2022.settings')
 
 application = get_asgi_application()
--- codestar2022/wsgi.py
+++ codestar2022/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PROJ_NAME project.
+WSGI config for codestar2022 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PROJ_NAME.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codestar2022.settings')
 
 application = get_wsgi_application()
